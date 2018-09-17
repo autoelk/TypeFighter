e4ab7a27f5197c9e81cc0b168a8f0cbf817eae90
lukefly2 <lukefly2@outlook.com>
2018-09-16 20:04:46 -0400

    Added Partial support for displaying cards

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ cards = {
     ["damage"] = {},
     ["mana"] = {},
     ["type"] = {},
+    ["elem"] = {},
     ["anim"] = {}
 }
 
@@ -24,10 +25,12 @@ function split(pString, pPattern)
         cap = pString:sub(last_end)
         table.insert(Table, cap)
     end
-    for i = 1, #Table do
-        print(Table[i])
-    end
     return Table
+end
+
+function love.wheelmoved(dx, dy)
+    velx = velx + dx * 20
+    vely = vely + dy * 20
 end
 
 function findCard(name)
@@ -56,12 +59,11 @@ function love.load()
     textBox = love.graphics.newImage("Assets/TextBox.png")
     titleFont = love.graphics.newFont("Assets/munro.ttf", 96)
     font = love.graphics.newFont("Assets/munro.ttf", 24)
-    title = "TypeFighter"
-    menu = "[P]lay Game"
+    --initialize variables
     input = ""
     message = "Type P to Start"
     gameStage = "menu"
-    --read cards into array cards
+    --read each card into array cards
     io.input("Assets/Cards/cards.txt")
     numCards = io.read()
     for i = 1, numCards do
@@ -70,11 +72,17 @@ function love.load()
         cards.damage[i] = tempTable[2]
         cards.mana[i] = tempTable[3]
         cards.type[i] = tempTable[4]
-        cards.anim[i] = newAnimation(love.graphics.newImage("Assets/Cards/" .. tempTable[5]), 160, 160, 1)
+        cards.elem[i] = tempTable[5]
+        cards.anim[i] = newAnimation(love.graphics.newImage("Assets/Cards/" .. tempTable[6]), 160, 160, 1)
     end
     io.close()
 
+    --allow repeating input
     love.keyboard.setKeyRepeat(true)
+    --scrolling
+    scrollSpeed = 10
+    posx, posy = love.graphics.getWidth() * 0.5, 300
+    velx, vely = 0, 0 -- The scroll velocity
 end
 
 function love.textinput(t)
@@ -119,32 +127,84 @@ function love.update(dt)
             cards.anim[i].currentTime = cards.anim[i].currentTime - cards.anim[i].duration
         end
     end
+    --scrolling
+    posx = posx + velx * scrollSpeed * dt
+    posy = posy + vely * scrollSpeed * dt
+
+    -- Gradually reduce the velocity to create smooth scrolling effect.
+    velx = velx - velx * math.min(dt * 10, 1)
+    vely = vely - vely * math.min(dt * 10, 1)
 end
 
 function love.draw()
     --background
     love.graphics.draw(background, 0, 0)
-    love.graphics.draw(textBox, 0, 570)
     love.graphics.setFont(font)
+    love.graphics.draw(person, 200, 400)
     --set font to normal font
-    love.graphics.printf(message, 5, 570, love.graphics.getWidth(), "left")
-    love.graphics.printf(input, 5, 570, love.graphics.getWidth(), "left")
     if gameStage == "menu" then
         --title
         love.graphics.draw(mainMenuTextBackground, 0, 205)
-        love.graphics.setFont(titleFont)
-        --set font to title font
-        love.graphics.printf(title, 0, 200, love.graphics.getWidth(), "center")
+        love.graphics.setFont(titleFont) --set font to title font
+        love.graphics.printf("TypeFighter", 0, 200, love.graphics.getWidth(), "center")
         --menu
         love.graphics.setFont(font)
-        love.graphics.printf(menu, 0, 300, love.graphics.getWidth(), "center")
+        love.graphics.printf("[P]lay Game", 0, 300, love.graphics.getWidth(), "center")
         --animation
         local spriteNum0 = math.floor(cards.anim[1].currentTime / cards.anim[1].duration * #cards.anim[1].quads) + 1
         local spriteNum1 = math.floor(cards.anim[2].currentTime / cards.anim[2].duration * #cards.anim[2].quads) + 1
         love.graphics.draw(cards.anim[1].spriteSheet, cards.anim[1].quads[spriteNum0], 50, 180, 0, 1)
         love.graphics.draw(cards.anim[2].spriteSheet, cards.anim[2].quads[spriteNum1], 750, 345, 3.14159, 1)
     end
-    love.graphics.draw(person, 200, 400)
+    if gameStage == "cardSelect" then --Stage of card selection
+        love.graphics.setColor(255, 255, 255) -- reset colors
+        --Display card select title
+        love.graphics.draw(mainMenuTextBackground, 0, posy - 150)
+        love.graphics.setFont(titleFont) --set font to title font
+        love.graphics.printf("Select Cards", 0, posy - 135, love.graphics.getWidth(), "center")
+
+        love.graphics.setFont(font)
+        local extraCards = numCards % 3
+        for i = 1, math.floor(numCards / 3) do
+            for j = 1, 3 do
+                if cards.elem[i + j - 1] == "fire" then
+                    love.graphics.setColor(232 / 255, 0 / 255, 43 / 255)
+                elseif cards.elem[i + j - 1] == "earth" then
+                    love.graphics.setColor(78 / 255, 171 / 255, 84 / 255)
+                elseif cards.elem[i + j - 1] == "water" then
+                    love.graphics.setColor(39 / 255, 98 / 255, 176 / 255)
+                else
+                    love.graphics.setColor(160 / 255, 160 / 255, 160 / 255)
+                end
+                love.graphics.rectangle("fill", 245 * (j - 1) + 65, 317 * (i - 1) + posy, 180, 252, 10)
+                love.graphics.setColor(255, 255, 255)
+                love.graphics.printf(cards.name[i + j - 1], 245 * (j - 1) + 65, 317 * (i - 1) + posy, 180, "left")
+                love.graphics.printf(cards.mana[i + j - 1], 245 * (j - 1) + 65, 317 * (i - 1) + posy, 180, "right")
+            end
+        end
+        --draw the extra cards
+        for i = 1, extraCards do
+            if cards.elem[math.floor(numCards / 3) + i] == "fire" then
+                love.graphics.setColor(232 / 255, 0 / 255, 43 / 255)
+            elseif cards.elem[math.floor(numCards / 3) + i] == "earth" then
+                love.graphics.setColor(78 / 255, 171 / 255, 84 / 255)
+            elseif cards.elem[math.floor(numCards / 3) + i] == "water" then
+                love.graphics.setColor(39 / 255, 98 / 255, 176 / 255)
+            else
+                love.graphics.setColor(160 / 255, 160 / 255, 160 / 255)
+            end
+            love.graphics.rectangle("fill", 245 * (i - 1) + 65, 317 * math.floor(numCards / 3) + posy, 180, 252, 10)
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.printf(cards.name[math.floor(numCards / 3) + i], 245 * (i - 1) + 65, math.floor(numCards / 3) + posy, 180, "left")
+            love.graphics.printf(cards.mana[math.floor(numCards / 3) + i], 245 * (i - 1) + 65, math.floor(numCards / 3) + posy, 180, "right")
+        end
+    end
+    if gameStage == "game" then
+    end
+    --input box at bottom of screen
+    love.graphics.draw(textBox, 0, 570)
+    love.graphics.printf(message, 5, 570, love.graphics.getWidth(), "left")
+    love.graphics.printf(input, 5, 570, love.graphics.getWidth(), "left")
 end
 
 function newAnimation(image, width, height, duration)
