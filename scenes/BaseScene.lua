7876126e9043e6cb8cc32d73a31c7feeb66b113c
Luke Tao <luketao68@gmail.com>
2026-03-26 12:34:14 -0400

    Add horizontal scrolling to menu input when exceeding bounds

--- scenes/BaseScene.lua
+++ scenes/BaseScene.lua
@@ -20,30 +20,51 @@ function BaseScene:draw() end
 function BaseScene:drawInputInterface()
     local ui = self.ctx.ui
     local inputRectHeight = 32
+    local inputY = GAME_HEIGHT - inputRectHeight
+    local leftPadding = 8
+    local rightPadding = 8
     lg.setColor(COLORS.BLACK)
-    lg.rectangle("fill", 0, GAME_HEIGHT - inputRectHeight, GAME_WIDTH, inputRectHeight)
+    lg.rectangle("fill", 0, inputY, GAME_WIDTH, inputRectHeight)
     
     local text = ui.input
     local color = COLORS.WHITE
+    local isTyping = ui.input ~= ""
     if text == "" then
         text = ui.messageLeft or ""
         color = COLORS.GREY
     end
     
     -- Blinking caret
-    if ui.input ~= "" then
+    if isTyping then
         local caretOn = math.floor(love.timer.getTime() * 2) % 2 == 0
         if caretOn then
             text = text .. "|"
         end
     end
     
-    lg.setFont(self.ctx.fonts.fontM)
+    local font = self.ctx.fonts.fontM
+    lg.setFont(font)
     lg.setColor(color)
-    lg.printf(text, 8, GAME_HEIGHT - inputRectHeight, GAME_WIDTH, "left")
+
+    local rightMessage = ui.messageRight or ""
+    local rightMessageWidth = font:getWidth(rightMessage)
+    local inputWidth = GAME_WIDTH - leftPadding - rightPadding - rightMessageWidth - 8
+    inputWidth = math.max(inputWidth, 0)
+
+    local drawX = leftPadding
+    local textWidth = font:getWidth(text)
+    local textOffsetX = 0
+    if isTyping and textWidth > inputWidth then
+        textOffsetX = inputWidth - textWidth -- Current horizontal scrolling of the input text
+    end
+
+    local prevScissorX, prevScissorY, prevScissorW, prevScissorH = lg.getScissor()
+    lg.setScissor(drawX, inputY, inputWidth, inputRectHeight)
+    lg.print(text, drawX + textOffsetX, inputY)
+    lg.setScissor(prevScissorX, prevScissorY, prevScissorW, prevScissorH)
 
     lg.setColor(COLORS.GREY)
-    lg.printf(ui.messageRight, -8, GAME_HEIGHT - inputRectHeight, GAME_WIDTH, "right")
+    lg.printf(rightMessage, -rightPadding, inputY, GAME_WIDTH, "right")
 end
 
 -- For single key presses
